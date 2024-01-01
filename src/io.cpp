rrrlasse/eXdupe

commit 9e0b3841adfe21d9702e0260f0d0cdd000f1905c

Attempt of trying to detect region locked files on Windows

src/io.cpp
@@ -96,6 +96,17 @@ size_t Cio::try_read(void *DstBuf, size_t Count, FILE *_File) {
     return Count;
 }
 
+// Call if you have prior tested that the file is long enough that the read will not exceed it
+size_t Cio::read_valid_length(void *DstBuf, size_t Count, FILE *_File, STRING name) {
+    size_t w = Cio::read((char *)DstBuf, Count, _File);
+    // Can be caused by region-locked files if on Windows, where it can occur anywhere inside
+    // the file. We do not want to attempt to discard compressed data that has already been written
+    // to the destination file (this is even impossible if compressing to stdout). So just abort. 
+    abort(_File > (FILE*)2 && w != Count, (UNITXT("Error reading file that has been opened successfully for reading - cannot recover: ") + name).c_str());
+    return w;
+}
+
+
 size_t Cio::write64(uint64_t i, FILE *_File) {
     uint64_t j = i;
     char c[8];

src/exdupe.cpp
@@ -8,7 +8,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
 #define VERSION_REVISION 0
-#define VERSION_BETA 18
+#define VERSION_BETA 19
 
 #define NOMINMAX
 #include <string>
@@ -1737,10 +1737,10 @@ void compress_file(const STRING &input_file, const STRING &filename, const bool 
 
         while(file_read < file_size) {
             statusbar(dup_counter_payload(), io.write_count, input_file);
-            size_t r = io.read(in, DISK_READ_CHUNK, ifile);
+            size_t r = io.read_valid_length(in, minimum(file_size - file_read, DISK_READ_CHUNK), ifile, input_file);
             if(input_file == UNITXT("-stdin") && r == 0) {
                 break;
-            }
+            }            
             file_read += r;
             payload_read += r;            
             checksum(in, r, &file_meta.ct);
@@ -1758,7 +1758,7 @@ void compress_file(const STRING &input_file, const STRING &filename, const bool 
     }
     else {
         assert(file_size <= DISK_READ_CHUNK - payload_queue.size());
-        size_t r = io.read(in, file_size, ifile);
+        size_t r = io.read_valid_length(in, file_size, ifile, input_file);       
         file_read += r;
         payload_read += r;
         checksum(in, r, &file_meta.ct);

src/io.hpp
@@ -45,6 +45,7 @@ class Cio {
     size_t write(const void *_Str, size_t _Count, FILE *_File);
     size_t try_write(const void *Str, size_t Count, FILE *_File);
     size_t try_read(void *DstBuf, size_t Count, FILE *_File);
+    size_t read_valid_length(void *DstBuf, size_t Count, FILE *_File, STRING name);
     size_t write64(uint64_t i, FILE *_File);
     size_t write32(unsigned int i, FILE *_File);
     size_t write8(char i, FILE *_File);

commit fc4e55d16665370d7409b82469fc0ee4176e4083

Fixed detection of stdin Regression by 9e0b384

src/io.cpp
@@ -98,11 +98,17 @@ size_t Cio::try_read(void *DstBuf, size_t Count, FILE *_File) {
 
 // Call if you have prior tested that the file is long enough that the read will not exceed it
 size_t Cio::read_valid_length(void *DstBuf, size_t Count, FILE *_File, STRING name) {
+#ifdef WINDOWS
+    bool is_std = _fileno(_File) == _fileno(stdin);
+#else
+    bool is_std = fileno(_File) == fileno(stdin);
+#endif
     size_t w = Cio::read((char *)DstBuf, Count, _File);
     // Can be caused by region-locked files if on Windows, where it can occur anywhere inside
     // the file. We do not want to attempt to discard compressed data that has already been written
     // to the destination file (this is even impossible if compressing to stdout). So just abort. 
-    abort(_File > (FILE*)2 && w != Count, (UNITXT("Error reading file that has been opened successfully for reading - cannot recover: ") + name).c_str());
+    abort(!is_std && w != Count, (UNITXT("Error reading file that has been opened successfully for reading - cannot recover: ") + name).c_str());
+
     return w;
 }
 