2ae5b02a74ae2dece150fc0d71f07a46d1239644
rrrlasse <exdupe@gmail.com>
2025-06-30 08:05:58 +0200

    Added abort.h

---
+++ src/abort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "utilities.hpp"
+
+#ifdef _WIN32
+void abort(bool b, int ret, const std::wstring &s);
+#endif
+
+void abort(bool b, int ret, const std::string &s);
+
+// todo, legacy
+void abort(bool b, const CHR *fmt, ...);
